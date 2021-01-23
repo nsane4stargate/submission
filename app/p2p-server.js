aacdf917898b8d116685e21c3c4e184972d068b3
Lea <nsane4stargate@gmail.com>
2021-01-22 20:34:29 -0700

    Modified the variable const socket = [] to const sockets in the P2pServer.js, due to error

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -6,7 +6,7 @@ const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 class P2pServer{
     constructor(blockchain){
         this.blockchain = blockchain;
-        this.socket = [];
+        this.sockets = [];
     }
 
     listen(){
@@ -30,8 +30,21 @@ class P2pServer{
         });
     }
     connectSocket(socket){
-        this.socket.push(socket);
+        this.sockets.push(socket);
         console.log('Socket Connected');
+
+        /* Sends socket with its data to the messageHandler() */
+        this.messageHandler(socket);
+        socket.send(JSON.stringify(this.blockchain.chain));
+    }
+
+    /* Event handler for newly connected socket */
+    messageHandler(socket){
+        
+        socket.on('message', message =>{
+            const data = JSON.parse(message);
+            console.log('data', data);
+        });
     }
 }
 
